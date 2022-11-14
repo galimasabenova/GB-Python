6aace72f6f612def60bdbe982afa508d77738bd2
galimasab <galima0405@gmail.com>
2022-11-14 17:25:47 +0600

    дз 4

---
+++ lesson_4/zp_sotr.py
@@ -0,0 +1,35 @@
+
+    "from sys import argv\n",
+    
+    "def zp_sotr(time_, stavka_, prem_):\n",
+    "    res = time_ * stavka_ + prem\n",
+    "    return res\n",
+   
+    "time_, stavka_, prem_ = argv\n",
+    "zp_ = zp_sotr(time_, stavka_, prem_)\n",
+    "    "
+   ]
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3 (ipykernel)",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.9.12"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
